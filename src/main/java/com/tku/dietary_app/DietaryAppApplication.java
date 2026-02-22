2d41584a5f4adc94e9507937d1560c2b9ac12073
JenLin Li <jolint514dc@gmail.com>
2026-02-22 19:01:55 +0800

    init: backend spring boot project

---
+++ src/main/java/com/tku/dietary_app/DietaryAppApplication.java
@@ -0,0 +1,13 @@
+package com.tku.dietary_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietaryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DietaryAppApplication.class, args);
+	}
+
+}
